31e8b1566b6c80d11d30bfb893ed80a69e8f6b15
Ogden <john.ogden@capgemini.com>
2017-10-08 15:10:32 +0100

    added sha1 hasher to miner

--- src/main/java/JOHacks/Miner/Miner.java
+++ src/main/java/JOHacks/Miner/Miner.java
@@ -1,6 +1,10 @@
 package JOHacks.Miner;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Formatter;
 
 import JOHacks.Generic.Key;
 import JOHacks.Generic.Transaction;
@@ -24,4 +28,35 @@ public class Miner {
 	public void RegisterMiner(Miner miner) {}
 	
 	public void mine(ArrayList<Transaction> transactions) {}
+	
+	public String calcHash(String toHash) {
+	    try
+	    {
+	        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+	        crypt.reset();
+	        crypt.update(toHash.getBytes("UTF-8"));
+	        return byteToHex(crypt.digest());
+	    }
+	    catch(NoSuchAlgorithmException e)
+	    {
+	        e.printStackTrace();
+	    }
+	    catch(UnsupportedEncodingException e)
+	    {
+	        e.printStackTrace();
+	    }
+	    return "";
+	}
+	
+	private static String byteToHex(final byte[] hash)
+	{
+	    Formatter formatter = new Formatter();
+	    for (byte b : hash)
+	    {
+	        formatter.format("%02x", b);
+	    }
+	    String result = formatter.toString();
+	    formatter.close();
+	    return result;
+	}
 }
--- src/test/java/JOHacks/AppTest.java
+++ src/test/java/JOHacks/AppTest.java
@@ -1,6 +1,7 @@
 package JOHacks;
 
 import JOHacks.Generic.KeyPair;
+import JOHacks.Miner.Miner;
 import JOHacks.Wallet.Wallet;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -40,5 +41,11 @@ public class AppTest
         Wallet wallet = new Wallet();
 
     	KeyPair keypair = wallet.GenerateKeyPair();    	
+    }
+
+    public void testMiner() {
+    	Miner miner = new Miner();
+    	String hash= miner.calcHash("hello world");
+    	assertEquals("2aae6c35c94fcfb415dbe95f408b9ce91ee846ed",hash);
     }
 }
