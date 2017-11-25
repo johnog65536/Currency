7c43ca2fa3772ead36a6249a99daf30d419db32e
domgoodwin <d0m182goodwin@gmail.com>
2017-11-25 17:08:10 +0000

    Add miner basic testing functionality

--- node-server/main.js
+++ node-server/main.js
@@ -1,9 +1,9 @@
 // imports
 var Blockchain = require('./blockchain');
-
+var Miner = require('./miner');
 var Transaction = require('./transaction'); // TEST PURPOSES PLEASE REMOVE
 
-// create blockchain
+
 var bc = new Blockchain("tester");
 bc.addBlock("I am a transaction");
 bc.addBlock("I also am");
@@ -17,10 +17,15 @@ for (var i = 0; i < hashList.length; i++) {
 
 console.log(bc.exportFile());
 bc.saveChain();
-// get blockchain
+
 
 
 var tx = new Transaction(10, "todoFrom", "todoTo", "Test please ignore");
 tx.confirm(bc.blockList[1].hashString, bc.blockList[1].blockID, bc.blockList[1].timestamp);
 console.log(tx);
+
+var mn = new Miner("tester");
+
+console.log(mn.blockchain);
+
 // add block
