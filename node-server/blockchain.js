7f68e2be8dd01827d6c6d2a688630379ae35e89b
domgoodwin <d0m182goodwin@gmail.com>
2017-11-25 19:02:18 +0000

    Add validation of blockchain methods

--- node-server/blockchain.js
+++ node-server/blockchain.js
@@ -1,6 +1,7 @@
 //
 var Block = require('./block');
 var fs = require('fs');
+const crypto = require('crypto')
 
 // Constructor
 function Blockchain(chainName, data) {
@@ -20,16 +21,16 @@ function Blockchain(chainName, data) {
     var inputObj = JSON.parse(data);
     this.creation = inputObj.creation_timestamp;
     this.lastUpdated = inputObj.last_updated;
-    this.blockList = mapBlockData(inputObj.blocks);
+    this.blockList = mapBlockData(inputObj['blocks']);
   }
 }
 
 function mapBlockData(blocks){
   var blockArr = [];
-  var blocksToMap = JSON.parse(JSON.stringify(blocks));
+  var blocksToMap = blocks;
   for (var i = 0; i < blocksToMap.length; i++) {
     var block = new Block();
-    var blockToMap = JSON.parse(blocksToMap[i]);
+    var blockToMap = blocksToMap[i];
     block.blockID = blockToMap.blockID;
     block.prevHash = blockToMap.prevHash;
     block.timestamp = blockToMap.timestamp;
@@ -94,10 +95,62 @@ Blockchain.prototype.exportFile = function(blockList){
     creation_timestamp: this.creation,
     last_updated: this.lastUpdated,
     chain_length: this.blockList.length,
-    blocks: blockList
+    blocks: this.blockList
   }
   return data;
 }
+
+// Basic blockchain validation
+// Uses next block prev hash and cur block hash
+Blockchain.prototype.validate = function() {
+  for (var i = 0; i < this.blockList.length; i++) {
+    // Get current block
+    var currentBlock = this.blockList[i];
+    if(!(i == this.blockList.length - 1)){
+      // Check next block
+      var nextBlock = this.blockList[i + 1];
+      if(currentBlock.getBlockHash() != nextBlock.prevHash){
+        console.error("Blockchain does not validate");
+        console.error("INVALID: \n" + currentBlock.getBlockHash() + "\n" + nextBlock.prevHash);
+        return false;
+      } else{
+        console.log("VALID: " + currentBlock.getBlockHash() + "//" + nextBlock.prevHash);
+      }
+    }
+  }
+  return true;
+}
+
+// Full blockchain validation
+// Does a new hash for each one to fully validate from start of chain
+Blockchain.prototype.validateFull = function() {
+  var toBeHashed = [];
+  var prevBlock = this.blockList[0];
+  for (var i = 0; i < this.blockList.length; i++) {
+    // Get current block
+    var currentBlock = this.blockList[i];
+    if(i == 0){
+      // Start with genesis block
+      continue;
+    } else{
+      prevBlock = this.blockList[i - 1];
+      //console.log("Comparing blocks: " + JSON.stringify(prevBlock) + JSON.stringify(currentBlock) + "\n");
+      var hash = crypto.createHash('sha256')
+      var prevBlockHash = prevBlock.getCompleteBlockHash();
+      hash.update(prevBlockHash + currentBlock.timestamp + currentBlock.nonce + currentBlock.tData);
+      var curBlockHash = hash.digest('hex');
+      if(curBlockHash != currentBlock.hashString){
+        console.error("Blockchain does not validate");
+        console.error("INVALID: \n" + curBlockHash + "\n" + currentBlock.hashString);
+        return false;
+      } else{
+        console.log("VALID: \n" + curBlockHash + "\n" + currentBlock.hashString);
+      }
+    }
+  }
+  return true;
+}
+
 
 
 
